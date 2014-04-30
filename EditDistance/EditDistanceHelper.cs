a1f282dc92b7650792c092a95aa7fe9d79cd5936
bondeluke <bondeluke@gmail.com>
2014-04-29 19:52:37 -0500

    Added EditDistance Project
    Implemented the Edit Distance algorithm.

---
+++ EditDistance/EditDistanceHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditDistance
+{
+    public delegate double Difference(char a, char b);
+
+    public static class EditDistanceHelper
+    {
+
+        public static double EditDistance(string x, string y, Difference Diff)
+        {
+            // Initialization
+            int m = x.Length;
+            int n = y.Length;
+            double[][] E = new double[m][];
+            for (int i = 0; i < m; i++)
+                E[i] = new double[n];
+
+            for (int i = 0; i < m; i++)
+                E[i][0] = i;
+
+            for (int j = 0; j < n; j++)
+                E[0][j] = j;
+
+            // Running the algorithm
+            for (int i = 1; i < m; i++)
+                for (int j = 1; j < n; j++)
+                    E[i][j] = Min(E[i - 1][j] + 1, E[i][j - 1] + 1, E[i - 1][j - 1] + Diff(x[i], y[j]));
+
+            return E[m - 1][n - 1];
+        }
+
+        static double Min(params double[] args)
+        {
+            var smallest = args[0];
+            for (int i = 0; i < args.Length; i++)
+                if (args[i] < smallest)
+                    smallest = args[i];
+
+            return smallest;
+        }
+
+
+    }
+}
---
+++ EditDistance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "anagram";
+            string y = "agnar";
+            var dist1 = EditDistanceHelper.EditDistance(x, y, Diff1);
+            var dist2 = EditDistanceHelper.EditDistance(x, y, Diff2);
+
+            System.Console.WriteLine(String.Format("The edit distance between {0} and {1} for part (a) is {2}", x, y, dist1));
+            System.Console.WriteLine(String.Format("The edit distance between {0} and {1} for part (b) is {2}", x, y, dist2));
+            System.Console.ReadLine();
+        }
+
+        static double Diff1(char a, char b)
+        {
+            if (a == b)
+                return 0;
+            return 1;
+        }
+
+        static double Diff2(char a, char b)
+        {
+            if (a == b)
+                return 0;
+            return 1 / 2;
+        }
+    }
+}
